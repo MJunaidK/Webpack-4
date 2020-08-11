eff542fa7d973e208f42ef608af0bc6d97c5b034
MJunaidK <junaidcts@gmail.com>
2020-08-11 16:58:44 -0400

    Extracting css into a separate bundle 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import helloWorld from './components/hello-world-btn/hello-world-button.js';
 import HelloWorldButton from './components/hello-world-btn/hello-world-button';
 import addImage from './add-image';
+import Heading from './components/Header/heading'
 
+const heading = new Heading();
+heading.render();
 const helloWorldButton =  new HelloWorldButton();
 helloWorldButton.render();
 //addImage();
