d318588dc1c62aadeb9952dcca193dcd6f07f814
MJunaidK <junaidcts@gmail.com>
2020-08-11 20:34:38 -0400

    Clean dist folder before generating new bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path =  require('path');
 const TerserPlugin =  require('terser-webpack-plugin');
 const MiniCssExtractPlugin =  require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -48,11 +50,16 @@ module.exports = {
          ]
     },
     plugins: [
+        new TerserPlugin(),
         new MiniCssExtractPlugin({
             filename: 'style.[contenthash].css'
         }),
-        new TerserPlugin(),
-        
+        new CleanWebpackPlugin({
+            cleanOnceBeforeBuildPatterns:[
+                '**/*', // Remove all the files from dist folder which is the default folder
+                path.join(process.cwd(), 'build/**/*') // For removing outside dist provide the absolute path.Removes all the files from build folder
+            ]
+        })
     
     ]
 }
