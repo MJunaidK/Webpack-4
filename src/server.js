af4d0680e323f5f92dcedbfc332b2ea2b14735af
MJunaidK <junaidcts@gmail.com>
2020-08-16 11:36:35 -0400

    Integrating express.js inot our application

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.get('/', function(req, res){
+    res.send('Some dummy content');
+})
+
+
+app.listen(3000, function(){
+    console.log('Application is running on http://localhost:3000/')
+})
