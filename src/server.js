c37adc0d95f5d32fcc0de9a57672361fe8edefe2
MJunaidK <junaidcts@gmail.com>
2020-08-16 12:39:44 -0400

    Integrating express.js inot a multiple js application

--- src/server.js
+++ src/server.js
@@ -3,9 +3,16 @@ const app = express();
 const path = require('path');
 const fs = require('fs');
 
-app.get('/', function(req, res){
+app.get('/hello-world/', function(req, res){
     //res.send('Some dummy content');
     const pathToHtmlFile =  path.resolve(__dirname,  '../dist/hello-world.html');
+    const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
+    res.send(contentFromHtmlFile);
+})
+
+app.get('/kiwi/', function(req, res){
+    //res.send('Some dummy content');
+    const pathToHtmlFile =  path.resolve(__dirname,  '../dist/kiwi.html');
     const contentFromHtmlFile = fs.readFileSync(pathToHtmlFile, 'utf-8');
     res.send(contentFromHtmlFile);
 })
